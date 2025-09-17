a6b420aa1bcee39560d9f492d3c620cde57c6156
juliocesarbal <juliocesarbaldiviezomoron@gmail.com>
2025-09-17 11:24:26 -0400

    Update API destination in next.config.ts to use environment variable

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ const nextConfig: NextConfig = {
     return [
       {
         source: '/api/:path*',
-        destination: 'http://localhost:3001/api/:path*',
+        destination: `${process.env.NEXT_PUBLIC_API_URL}/:path*`,
       },
     ]
   },
